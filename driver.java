d4605b0379a21c87dd8a4cecf47291e5e6af61a5
mihirjadhav7890 <95160156+mihirjadhav7890@users.noreply.github.com>
2022-12-09 10:53:57 +0530

    Add files via upload

---
+++ driver.java
@@ -0,0 +1,30 @@
+package packages;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class driver {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter your buying price per share.");
+		double buyingPrice = scan.nextDouble();
+		
+		int day =1;
+		double closingPrice = 0.1;
+		DecimalFormat df = new DecimalFormat("0.00");
+		while(true) {
+			System.out.println("enter the closeing price for day"+day+"(any negative value to leave:)");
+			closingPrice = scan.nextDouble();
+			if(closingPrice<0.0)break;
+			double earnings= closingPrice - buyingPrice;
+			if(earnings > 0) {
+				System.out.println("After day"+day+ ", you earned"+df.format(earnings)+" per share.");
+			}else if(earnings < 0.0) {
+				System.out.println("After day"+day+ ", you lost"+df.format(-earnings)+" per share.");
+			}else{
+				System.out.println("After day"+day+ ", you have"+" no earning per share.");
+			}
+			day +=1;
+		}
+		scan.close();
+	}
+}
